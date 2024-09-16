8304f28b665f48f6f57d1aabad575eb6166460ef
Kieran Collins <kiecol@protonmail.com>
2024-09-16 20:09:58 +0000

    Env change

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ["SECRET_KEY"] = "your_flask_secret_key"
-os.environ["OPENAI_API_KEY"] = "your_openai_api_key"
-# Flask Mail
-os.environ["MAIL_USERNAME"] = "your_email_username"
-os.environ["MAIL_PASSWORD"] = "your_email_password"
